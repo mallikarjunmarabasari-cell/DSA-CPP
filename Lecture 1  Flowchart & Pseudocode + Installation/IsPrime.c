02e789ab9959194da38761cfe23278d3ca4f5531
Mallikarjun S M <mallikarjunmarabasari@gmail.com>
2026-07-13 23:39:48 +0530

    Refactor code structure for improved readability and maintainability

--- Lecture 1  Flowchart & Pseudocode + Installation/IsPrime.c
+++ Lecture 1  Flowchart & Pseudocode + Installation/IsPrime.c
@@ -6,19 +6,12 @@ int main(void) {
     printf("Enter a number: ");
     scanf("%d", &n);
 
-    if (n <= 1) {
-        printf("%d is non-prime\n", n);
-        return 0;
-    }
-
-    while (i <= (n - 1)) {
-        if (n % i == 0) {
-            printf("%d is non-prime\n", n);
-            return 0;
+    while(i <= (n - 1)){
+        if(n % i == 0){
+            printf("%d is a Non-prime number",n);
+            break;
         }
-        i = i + 1;
+        i+=1;
     }
-
-    printf("%d is Prime\n", n);
-    return 0;
+    printf("%d is a Prime number",n);
 }
