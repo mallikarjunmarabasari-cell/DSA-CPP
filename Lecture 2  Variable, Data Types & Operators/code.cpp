a55a958f619d20928342b800e45985eb91a5b19b
Mallikarjun S M <mallikarjunmarabasari@gmail.com>
2026-07-16 23:21:55 +0530

    Fix syntax error in output statement and improve code formatting in Variable, Data Types & Operators example

--- Lecture 2  Variable, Data Types & Operators/code.cpp
+++ Lecture 2  Variable, Data Types & Operators/code.cpp
@@ -3,5 +3,11 @@ using namespace std;
 int main(){
     cout<<"Hello World!" << endl;
     cout<<"Hello World!" << "\n";
+    int age = 22;
+    cout<<age <<endl;
+    cout<< sizeof(age); // 4 bytes
+    cout<< sizeof(int); // 4 bytes
+    char grade = 'A';
+    cout<<"\n" grade;
     return 0;
 }
